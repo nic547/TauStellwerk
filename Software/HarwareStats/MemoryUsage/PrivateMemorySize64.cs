4c9f377295a06ddd69db0fb68869a29dce1a4ac4
nic547 <7825979+nic547@users.noreply.github.com>
2021-02-16 21:07:03 +0100

    Add a IInfoProvider that provides PrivateMemorySize64
    Should work on all Platforms.

---
+++ Software/HarwareStats/MemoryUsage/PrivateMemorySize64.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="PrivateMemorySize64.cs" company="Dominic Ritz">
+// Copyright (c) Dominic Ritz. All rights reserved.
+// Licensed under the GNU GPL license. See LICENSE file in the project root for full license information.
+// </copyright>
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using JetBrains.Annotations;
+using PiStellwerk.Util;
+
+namespace HardwareInfo.MemoryUsage
+{
+    /// <summary>
+    /// Reads the PrivateMemorySize64 of the current process. Should not fail on any Platform.
+    /// </summary>
+    [UsedImplicitly]
+    public class PrivateMemorySize64 : IInfoProvider
+    {
+        /// <inheritdoc />
+        public bool CheckAvailability()
+        {
+            try
+            {
+                _ = GetInfoRecords();
+                ConsoleService.PrintMessage("PrivateMemorySize64 is available on this device");
+                return true;
+            }
+            catch (Exception)
+            {
+                ConsoleService.PrintError("PrivateMemorySize64 is NOT available on this device. This should not happen!");
+                return false;
+            }
+        }
+
+        /// <inheritdoc />
+        public IList<InfoRecord> GetInfoRecords()
+        {
+            return new List<InfoRecord>
+            {
+                new InfoRecord()
+                {
+                    Name = "PrivateMemorySize64",
+                    Type = InfoType.MemoryUsage,
+                    Value = Process.GetCurrentProcess().PrivateMemorySize64.ToString(),
+                },
+            };
+        }
+    }
+}
