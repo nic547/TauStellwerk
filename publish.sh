79d357d6a2dd2857e61223bcb21f789bac918239
nic547 <7825979+nic547@users.noreply.github.com>
2021-08-14 11:24:38 +0200

    add a basic script for publishing the application

---
+++ publish.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+customargs="-c Release /property:PublishTrimmed=True /p:DebugSymbols=false /p:DebugType=None"
+dotnet publish ./src/TauStellwerk.Server/ -r linux-x64 -o ./publish/linux-x64 $customargs
+dotnet publish ./src/TauStellwerk.Server/ -r linux-arm64 -o ./publish/linux-arm64 $customargs
+dotnet publish ./src/TauStellwerk.Server/ -r win-x64 -o ./publish/windows-x64 $customargs
+dotnet publish ./src/TauStellwerk.Server/ -r win-arm64 -o ./publish/windows-arm64 $customargs
+rm ./publish/*/*.xml
+rm ./publish/*/appsettings.Development.json
+rm -r ./publish/*/BlazorDebugProxy
+rm ./publish/*/web.config
