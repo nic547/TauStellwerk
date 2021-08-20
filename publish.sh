120832773d4233bcae1844165e663fb2c8e5e9b6
Nic547 <7825979+nic547@users.noreply.github.com>
2021-08-20 17:35:59 +0200

    Update build.sh, only use PublishSingeFile in Relase mode
    Also disable PublishSingleFile for CI

--- publish.sh
+++ publish.sh
@@ -1,15 +1,27 @@
-#!/bin/sh
-
-rm -rf ./publish
-
+#!/bin/bash
+set -e
 
 customargs="-c Release /property:PublishTrimmed=True /p:DebugSymbols=false /p:DebugType=None"
-dotnet publish ./src/TauStellwerk.Server/ -r linux-x64 -o ./publish/linux-x64 $customargs
-dotnet publish ./src/TauStellwerk.Server/ -r linux-arm64 -o ./publish/linux-arm64 $customargs
-dotnet publish ./src/TauStellwerk.Server/ -r win-x64 -o ./publish/windows-x64 $customargs
-dotnet publish ./src/TauStellwerk.Server/ -r win-arm64 -o ./publish/windows-arm64 $customargs
+rids=("linux-x64" "linux-arm64" "win-x64" "win-arm64")
 
-rm ./publish/*/*.xml
-rm ./publish/*/appsettings.Development.json
-rm -r ./publish/*/BlazorDebugProxy
-rm ./publish/*/web.config
+BLUE='\033[0;34m'
+NC='\033[0m' # No Color
+
+rm -rf ./publish
+dotnet clean /p:PublishSingleFile=false
+
+for rid in ${rids[@]}
+
+do
+echo -e "$BLUE --- Building $rid --- $NC"
+dotnet publish ./src/TauStellwerk.Server/ -r $rid -o ./publish/$rid $customargs
+
+rm ./publish/$rid/*.xml
+rm ./publish/$rid/appsettings.Development.json
+rm -r ./publish/$rid/BlazorDebugProxy
+rm ./publish/$rid/web.config
+
+done
+
+echo -e "$BLUE --- All builds completed! --- $NC"
+
