361f795078e4bdc6901ce2dd46003ec96e9ab541
nic547 <7825979+nic547@users.noreply.github.com>
2021-10-02 21:45:56 +0200

    Update publish.sh with optional parameter for location of dotnet
    If desired, a explicit path (like "/home/user/.dotnet/dotnet") can be provided to the buildscript.

--- publish.sh
+++ publish.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 
+dotnet=${1:-dotnet}
+
 customargs="-c Release /property:PublishTrimmed=True /p:DebugSymbols=false /p:DebugType=None /p:TrimMode=link"
 rids=("linux-x64" "linux-musl-x64" "linux-arm64" "linux-musl-arm64" "win-x64" "win-arm64")
 
@@ -8,13 +10,13 @@ BLUE='\033[1;34m'
 NC='\033[0m' # No Color
 
 rm -rf ./publish
-dotnet clean /p:PublishSingleFile=false -c Release
+$dotnet clean /p:PublishSingleFile=false -c Release
 
 for rid in ${rids[@]}
 
 do
 echo -e "$BLUE --- Building $rid --- $NC"
-dotnet publish ./src/TauStellwerk.Server/ -r $rid -o ./publish/$rid $customargs
+$dotnet publish ./src/TauStellwerk.Server/ -r $rid -o ./publish/$rid $customargs
 
 rm ./publish/$rid/*.xml
 rm ./publish/$rid/appsettings.Development.json
