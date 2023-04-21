d9762542f0e090605206d9d878db252aa1aa791b
nic547 <7825979+nic547@users.noreply.github.com>
2023-04-21 23:25:21 +0200

    Remove BOM from files

--- src/TauStellwerk.Desktop/ViewModels/SettingsViewModel.cs
+++ src/TauStellwerk.Desktop/ViewModels/SettingsViewModel.cs
@@ -1,4 +1,4 @@
-﻿// <copyright file="SettingsViewModel.cs" company="Dominic Ritz">
+// <copyright file="SettingsViewModel.cs" company="Dominic Ritz">
 // Copyright (c) Dominic Ritz. All rights reserved.
 // Licensed under the GNU GPL license. See LICENSE file in the project root for full license information.
 // </copyright>
