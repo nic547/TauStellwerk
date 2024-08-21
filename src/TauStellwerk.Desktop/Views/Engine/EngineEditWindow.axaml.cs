1aa2062c1a4f08b74460e3b3cf78bd14008f9935
nic547 <7825979+nic547@users.noreply.github.com>
2024-08-21 22:17:22 +0200

    Desktop: Set focus to newly added function when editing engines

--- src/TauStellwerk.Desktop/Views/Engine/EngineEditWindow.axaml.cs
+++ src/TauStellwerk.Desktop/Views/Engine/EngineEditWindow.axaml.cs
@@ -1,9 +1,11 @@
 ﻿// This file is part of the TauStellwerk project.
 //  Licensed under the GNU GPL license. See LICENSE file in the project root for full license information.
 
-using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Presenters;
 using Avalonia.Input;
-using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+using Avalonia.LogicalTree;
 using JetBrains.Annotations;
 using TauStellwerk.Desktop.Controls;
 using TauStellwerk.Desktop.ViewModels;
@@ -22,10 +24,6 @@ public partial class EngineEditWindow : BaseWindow
         vm.ClosingRequested += Close;
 
         InitializeComponent();
-
-#if DEBUG
-        this.AttachDevTools();
-#endif
     }
 
     [UsedImplicitly]
@@ -36,11 +34,6 @@ public partial class EngineEditWindow : BaseWindow
         // https://github.com/AvaloniaUI/Avalonia/issues/2593
     }
 
-    private void InitializeComponent()
-    {
-        AvaloniaXamlLoader.Load(this);
-    }
-
     private void TagInput_KeyDown(object? sender, KeyEventArgs e)
     {
         if (e.Key == Key.Enter)
@@ -48,4 +41,36 @@ public partial class EngineEditWindow : BaseWindow
             _vm.AddTagCommand.Execute(null);
         }
     }
+
+    private void Functions_KeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.Enter)
+        {
+            _vm.AddFunctionCommand.Execute(null);
+            FunctionItemsControl.LayoutUpdated += SetFocusToLastFunctionField;
+        }
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        FunctionItemsControl.LayoutUpdated += SetFocusToLastFunctionField;
+    }
+
+    private void SetFocusToLastFunctionField(object? obj, EventArgs? args)
+    {
+        // hideous thing - we need to subscribe to the ContentPresenter to know when it acutally loaded the UI elements and isn't empty
+        // We can only do that after the ContentPresenter actually exists, and we need to clean up the subscriptions so the focus is only set once.
+        var lastContentPresenter = FunctionItemsControl.GetLogicalChildren().Last() as ContentPresenter ?? throw new NullReferenceException("Failed to get content presenter");
+        lastContentPresenter.Loaded += ContentPresenterOnLoaded;
+        FunctionItemsControl.LayoutUpdated -= SetFocusToLastFunctionField;
+    }
+
+    private void ContentPresenterOnLoaded(object? sender, RoutedEventArgs e)
+    {
+        var contentPresenter = (ContentPresenter)sender!;
+        (contentPresenter.Child as StackPanel)?.Children.OfType<TextBox>().Single().Focus();
+        contentPresenter.Loaded -= ContentPresenterOnLoaded;
+    }
+
+
 }
